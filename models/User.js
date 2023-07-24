9ae15e76bc983206c39011b40da67f6aae30da85
Natasa <nkilibarda3@gmail.com>
2023-07-24 16:49:16 +1000

    Add user model

--- models/User.js
+++ models/User.js
@@ -1,33 +1,54 @@
-const { Schema, model } = require('mongoose');
-const assignmentSchema = require('./Assignment');
+const { Schema, model } = require("mongoose");
+const thoughtSchema = require("./Thought");
 
 // Schema to create User model
 const userSchema = new Schema(
   {
-    first: {
+    username: {
       type: String,
+      unique: true,
       required: true,
-      max_length: 50,
+      trim: true,
     },
-    last: {
+    email: {
       type: String,
+      unique: true,
       required: true,
-      max_length: 50,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
     },
     github: {
       type: String,
       required: true,
       max_length: 50,
     },
-    assignments: [assignmentSchema],
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
   },
   {
     toJSON: {
       getters: true,
+      virtuals: true,
     },
   }
 );
 
-const Student = model('student', studentSchema);
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
 
-module.exports = Student;
+const User = model("user", userSchema);
+
+module.exports = User;
