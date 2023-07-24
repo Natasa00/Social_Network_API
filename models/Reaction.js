a09fc00423d2c380a0b1b936fc24e7c25badf208
Natasa <nkilibarda3@gmail.com>
2023-07-24 16:49:30 +1000

    Add Thought + Reaction models

---
+++ models/Reaction.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      max_length: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (date) => {
+        if (date) return date.toISOString();
+      },
+    },
+    endDate: {
+      type: Date,
+      default: () => new Date(+new Date() + 84 * 24 * 60 * 60 * 1000),
+    },
+    students: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Student',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = reactionSchema;
